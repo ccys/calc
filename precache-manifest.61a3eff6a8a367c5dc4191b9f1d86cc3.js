95feeeee0f32785784ab0de8642a32fcf36cb037
caichenyue <caichenyue@mansuo.com>
2020-05-11 15:59:57 +0800

    Updates

---
+++ precache-manifest.61a3eff6a8a367c5dc4191b9f1d86cc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604ab7ceb8e75b1f2ce24a9072ec3e42",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "3a0faed5190c44ca74f4",
+    "url": "/calc/static/css/2.d8e6d793.chunk.css"
+  },
+  {
+    "revision": "556d231b4d32fc43709f",
+    "url": "/calc/static/css/main.1fc31612.chunk.css"
+  },
+  {
+    "revision": "3a0faed5190c44ca74f4",
+    "url": "/calc/static/js/2.4b07e8e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/calc/static/js/2.4b07e8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556d231b4d32fc43709f",
+    "url": "/calc/static/js/main.6ab8e1d8.chunk.js"
+  },
+  {
+    "revision": "50bc324973ce120fe115",
+    "url": "/calc/static/js/runtime-main.038f2cec.js"
+  }
+]);
