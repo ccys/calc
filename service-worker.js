813c961024d0a2859d4be966ced1ec4da2a951e0
caichenyue <caichenyue@mansuo.com>
2020-11-03 22:41:58 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e93f0ddb2d838a6f42455d5915ed9e7.js"
+  "/precache-manifest.6aa1c9d80d3844704486d5c0dda7d0c5.js"
 );
 
 self.addEventListener('message', (event) => {
